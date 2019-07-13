6c06762b3256b52ee41920d05f76694f54f93446
Gharghi, Shahin <shahin@asiatech.ir>
2019-07-13 16:49:12 +0430

    Changes

--- web/apps/backend/management/commands/import_updates.sh
+++ web/apps/backend/management/commands/import_updates.sh
@@ -5,6 +5,6 @@ dir=/shahin/BGPMon/web/apps/backend
 rm $dir/tmp/latest-update.gz
 rm $dir/tmp/out.csv
 wget -P $dir/tmp/ http://data.ris.ripe.net/rrc00/latest-update.gz
-bgpdump -m $dir/tmp/latest-update.gz | awk  -F '|' {' if ($3 == "A") print $2 "|" $5 "|" $6 "|" $7 "|" $12'} > $dir/tmp/out.csv
+/usr/bin/bgpdump/bgpdump -m $dir/tmp/latest-update.gz | awk  -F '|' {' if ($3 == "A") print $2 "|" $5 "|" $6 "|" $7 "|" $12'} > $dir/tmp/out.csv
 mysql -u root -p"0300301" -D bgpmon -e "truncate table main_app_dump"
 mysql -u root -p"0300301" -D bgpmon -e "LOAD DATA INFILE '$dir/tmp/out.csv' INTO TABLE main_app_dump FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n' IGNORE 1 LINES (time, asn, @var1, path, community) SET network = INET6_ATON(SUBSTRING(@var1, 1, POSITION('/' in @var1) - 1)); "

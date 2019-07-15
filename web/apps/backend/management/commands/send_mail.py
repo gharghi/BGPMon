72f02ca52579facd80d3b1e2ca0aa7cee1cf4186
Gharghi, Shahin <shahin@asiatech.ir>
2019-07-15 18:03:18 +0430

    Bugfix: Adding Prefix item into dump and using it in sending mails

--- web/apps/backend/management/commands/send_mail.py
+++ web/apps/backend/management/commands/send_mail.py
@@ -1,19 +1,19 @@
-import time
 import requests
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.db.models import Q
-from web.apps.main_app.models import Notifications, NotificationRule
-from web.apps.jwt_store.models import User
 from django.core.management.base import BaseCommand
+
+from web.apps.jwt_store.models import User
+from web.apps.main_app.models import Notifications, NotificationRule
+
 
 def status(text):
     code = {
-        1:"Has been transited",
-        2:"Has been hijacked",
-        3:"Is Transiting",
-        4:"Is Hijacking",
-        0:"Path has been changed",
+        1: "Has been transited",
+        2: "Has been hijacked",
+        3: "Is Transiting",
+        4: "Is Hijacking",
+        0: "Path has been changed",
     }
     return code[text]
 
@@ -26,7 +26,7 @@ def get_notifications(user):
         # notifications = Notifications.objects.filter(Q(user__id=user.id) | Q(time__range=[start_date, end_date]))
         notifications = Notifications.objects.filter(status=0).filter(user__id=user.id).order_by('time').reverse()
         mail = "BGPMon Alerts\n"
-        if notifications.exists(): ##prevent sending empty notification
+        if notifications.exists():  ##prevent sending empty notification
             for notification in notifications:
                 if notification.type == 1 and not rule.transited:
                     continue
@@ -36,7 +36,8 @@ def get_notifications(user):
                     continue
                 if notification.type == 4 and not rule.hijacking:
                     continue
-                mail = mail + str(notification.prefix) + '\t\t' + status(notification.type) + '\t\t\t ' + str(notification.path) + '\n'
+                mail = mail + str(notification.prefix) + '\t\t' + status(notification.type) + '\t\t\t ' + str(
+                    notification.path) + '\n'
 
             if rule.email:
                 email = EmailMessage('BGPMon Alert', mail, to=[rule.email])
@@ -44,8 +45,9 @@ def get_notifications(user):
 
             if rule.telegram:
                 url = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (
-                   settings.TELEGRAM_BOT_KEY , '-1001425080891', mail)
+                    settings.TELEGRAM_BOT_KEY, '-1001425080891', mail)
                 requests.get(url, timeout=10)
+
 
 class Command(BaseCommand):
     help = 'Sending notifications to users'

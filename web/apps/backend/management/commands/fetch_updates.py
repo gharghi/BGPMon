2bc15756d892b956003d38e07a71df72b683f45a
Gharghi, Shahin <shahin@asiatech.ir>
2019-07-22 21:43:44 +0430

    Bugfix: add try except

--- web/apps/backend/management/commands/fetch_updates.py
+++ web/apps/backend/management/commands/fetch_updates.py
@@ -52,10 +52,14 @@ class Command(BaseCommand):
             path = update['path'].split(' ')[::-1]
             asn = path[0]
             upstream = path[1]
-            query = "select neighbors.left as left_neighbor, neighbors.right as right_neighbor, asn.asn as asn, asn.user_id as user " \
-                    "from main_app_neighbors as neighbors inner join main_app_asn as asn on neighbors.asn_id = asn.id where " \
-                    "asn.asn = '" + str(asn) + "' and neighbors.left = " + str(upstream)
-            cs.execute(query)
+            try:
+                query = "select neighbors.left as left_neighbor, neighbors.right as right_neighbor, asn.asn as asn, asn.user_id as user " \
+                        "from main_app_neighbors as neighbors inner join main_app_asn as asn on neighbors.asn_id = asn.id where " \
+                        "asn.asn = '" + str(asn) + "' and neighbors.left = " + str(upstream)
+                cs.execute(query)
+            except Exception as e:
+                print(e)
+
             if not cs.rowcount:
                 notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': update['prefix'],
                                 'type': 1}
@@ -64,10 +68,13 @@ class Command(BaseCommand):
 
             # Checking if prefix is announcing with other ASBs that are not in database
             prefix = update['prefix']
-            query = "select origins.origin as origin, prefix.prefix as prefix, prefix.user_id as user from main_app_origins as origins " \
-                    "inner join main_app_prefix as prefix on origins.prefix_id = prefix.id where prefix.prefix = '" + prefix + \
-                    "' and origins.origin = " + str(asn)
-            cs.execute(query)
+            try:
+                query = "select origins.origin as origin, prefix.prefix as prefix, prefix.user_id as user from main_app_origins as origins " \
+                        "inner join main_app_prefix as prefix on origins.prefix_id = prefix.id where prefix.prefix = '" + prefix + \
+                        "' and origins.origin = " + str(asn)
+                cs.execute(query)
+            except Exception as e:
+                print(e)
             if not cs.rowcount:
                 notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
                                 'type': 2}
@@ -75,9 +82,12 @@ class Command(BaseCommand):
 
         # Searching announced ASNs in our database
         updates = []
-        query = "select distinct main_app_asn.asn as asn, main_app_dump.path as path, INET6_NTOA(main_app_dump.network) as prefix, main_app_dump.time as time from main_app_dump inner join main_app_asn on main_app_asn.asn in (SUBSTRING_INDEX( main_app_dump.path, ' ', 100))"
-        cs.execute(query)
-        rows = cs.fetchall()
+        try:
+            query = "select distinct main_app_asn.asn as asn, main_app_dump.path as path, INET6_NTOA(main_app_dump.network) as prefix, main_app_dump.time as time from main_app_dump inner join main_app_asn on main_app_asn.asn in (SUBSTRING_INDEX( main_app_dump.path, ' ', 100))"
+            cs.execute(query)
+            rows = cs.fetchall()
+        except Exception as e:
+            print(e)
         # print (len(rows))
         for item in rows:
             updates.append({'asn': item[0], 'path': item[1], 'prefix': item[2], 'time': item[3]})
@@ -86,39 +96,41 @@ class Command(BaseCommand):
             asn = str(update['asn'])
             path = update['path'].split(' ')[::-1]
             prefix = update['prefix']
+            try:
+                if path.index(asn) is 0:
+                    # Checking if upstream provider is in left asns in database
+                    upstream = path[1]
+                    query = "select neighbors.left as left_neighbor, neighbors.right as right_neighbor, asn.asn as asn, asn.id as asn_id, asn.user_id as user from main_app_neighbors as neighbors inner join main_app_asn as asn on neighbors.asn_id = asn.id where asn.asn = " + str(
+                        asn) + " and neighbors.left = " + str(upstream)
+                    # print(query)
+                    cs.execute(query)
+                    if not cs.rowcount:
+                        notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
+                                        'type': 1}
+                        print("error, has been transited ", insert_notifications(notification))
 
-            if path.index(asn) is 0:
-                # Checking if upstream provider is in left asns in database
-                upstream = path[1]
-                query = "select neighbors.left as left_neighbor, neighbors.right as right_neighbor, asn.asn as asn, asn.id as asn_id, asn.user_id as user from main_app_neighbors as neighbors inner join main_app_asn as asn on neighbors.asn_id = asn.id where asn.asn = " + str(
-                    asn) + " and neighbors.left = " + str(upstream)
-                # print(query)
-                cs.execute(query)
-                if not cs.rowcount:
-                    notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
-                                    'type': 1}
-                    print("error, has been transited ", insert_notifications(notification))
+                    # Checking if ASN is advertising prefix that is not in database
+                    query = "select prefix.prefix as prefix, origins.origin as origin, prefix.user_id as user from main_app_prefix as prefix inner join main_app_origins as origins on origins.prefix_id = prefix.id where prefix.prefix = '" + prefix + "' and origins.origin = " + str(
+                        asn)
+                    cs.execute(query)
+                    if not cs.rowcount:
+                        notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
+                                        'type': 4}
+                        print("error, is hijacking ", insert_notifications(notification))
 
-                # Checking if ASN is advertising prefix that is not in database
-                query = "select prefix.prefix as prefix, origins.origin as origin, prefix.user_id as user from main_app_prefix as prefix inner join main_app_origins as origins on origins.prefix_id = prefix.id where prefix.prefix = '" + prefix + "' and origins.origin = " + str(
-                    asn)
-                cs.execute(query)
-                if not cs.rowcount:
-                    notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
-                                    'type': 4}
-                    print("error, is hijacking ", insert_notifications(notification))
-
-            elif path.index(asn) is 1:
-                # Checking if right ASNs are in database as right hand
-                right = path[0]
-                query = "select neighbors.right as right_neighbor, asn.asn as asn, asn.user_id as user from main_app_neighbors as neighbors inner join main_app_asn as asn on neighbors.asn_id = asn.id where asn.asn = " + str(
-                    path[1]) + " and neighbors.right = " + str(right)
-                cs.execute(query)
-                if not cs.rowcount:
-                    notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
-                                    'type': 3}
-                    print("error, is transiting ", insert_notifications(notification))
-            else:
-                print("info, upstream path has changed")
+                elif path.index(asn) is 1:
+                    # Checking if right ASNs are in database as right hand
+                    right = path[0]
+                    query = "select neighbors.right as right_neighbor, asn.asn as asn, asn.user_id as user from main_app_neighbors as neighbors inner join main_app_asn as asn on neighbors.asn_id = asn.id where asn.asn = " + str(
+                        path[1]) + " and neighbors.right = " + str(right)
+                    cs.execute(query)
+                    if not cs.rowcount:
+                        notification = {'path': update['path'], 'time': update['time'], 'asn': asn, 'prefix': prefix,
+                                        'type': 3}
+                        print("error, is transiting ", insert_notifications(notification))
+                else:
+                    print("info, upstream path has changed")
+            except Exception as e:
+                print(e)
         # run the send mail command
         # return render(request, 'notify/send_mail.html')
